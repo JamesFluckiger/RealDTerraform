664529c6fb4a47d0a38ffa28bf7d91c4174f7533
Jamesafluke <jamesafluke@gmail.com>
2022-03-31 19:53:21 -0600

    Create terraform directory structure

---
+++ modules/DeployVnet/variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+    type = string
+}
+
+variable "rg_name" {
+    type = string
+}
+
+variable "vnet_name" {
+    type = string
+}
+
+variable "address_space" {
+    type = list
+}
+
